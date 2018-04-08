8cd057c79c4e4d0b44567dbe776c0dde238e9046
Matt Kirwan <matthew.kirwan@skybettingandgaming.com>
2018-04-08 20:48:55 +0100

    Launch new window practice

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,34 +1,64 @@
+import javax.swing.{JDialog, JFrame, UIManager}
+
 import swing._
 import event._
 
-object ChatApp extends SimpleSwingApplication {
-  def top = new MainFrame {
-    title = "Chat Shit Get Banged"
 
-    object text extends TextField { columns = 5 }
+class LaunchGame {
+  def showNewWindow() = {
+    val frame: Frame = new GameFrame()
+  }
 
-    val label = new Label {
-      text = "default"
+  def createButtonPane(f: Frame): FlowPanel = {
+    val button = new Button("Launch Game")
+    f.listenTo(button)
+    f.reactions += {
+      case ButtonClicked(`button`) => showNewWindow()
     }
-
-    val button = new Button {
-      text = "Send"
-    }
-
-    contents = new BoxPanel(Orientation.Vertical) {
-      contents += text
+    new FlowPanel() {
+      border = Swing.EmptyBorder(5,5,5,5)
       contents += button
-      contents += label
-      border = Swing.EmptyBorder(30,30,10,30)
     }
-
-    listenTo(button)
-
-    var nClicks = 0
-    reactions += {
-      case ButtonClicked(b) =>
-        label.text = text.text
-    }
-
   }
 }
+
+
+class GameFrame extends Frame {
+  title = "Battleships"
+  val button = new Button {
+    text = "Test Button"
+  }
+  visible = true
+  contents = new BoxPanel(Orientation.Vertical) {
+    contents += button
+  }
+}
+
+
+object LaunchGame extends SimpleSwingApplication {
+  lazy val top: Frame { val demo: LaunchGame } = new Frame() {
+    title = "FrameDemo2"
+    //Use the Java look and feel.  This needs to be done before the frame is created
+    //so the companion object FrameDemo2 cannot simply extend SimpleSwingApplcation.
+    try {
+      UIManager.setLookAndFeel(
+        UIManager.getCrossPlatformLookAndFeelClassName());
+    } catch {
+      case e: Exception => ;
+    }
+    //Make sure we have nice window decorations.
+    JFrame.setDefaultLookAndFeelDecorated(true);
+    JDialog.setDefaultLookAndFeelDecorated(true);
+    //Create and set up the content pane.
+    val demo = new LaunchGame()
+  }
+
+  val bp: BorderPanel = new BorderPanel() {
+    layout(top.demo.createButtonPane(top)) = BorderPanel.Position.South
+  }
+  top.contents = bp
+
+}
+
+
+
