778b0c007197b90ba27f4a451f019e1f8b7d6c46
Matt Kirwan <matthew.kirwan@skybettingandgaming.com>
2018-04-08 21:16:00 +0100

    Added some comments to clarify what it is I've done

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,37 @@
-import javax.swing.{JDialog, JFrame, UIManager}
 
 import swing._
 import event._
 
+// The main entry point
+object LaunchGame extends SimpleSwingApplication {
 
+  // top() is a Swing applications main equivalent. Override it with our custom game launcher and also let it lazy load.
+  lazy val top: Frame { val gameLauncher: LaunchGame } = new Frame() {
+    title = "Launch Game"
+    val gameLauncher = new LaunchGame()
+  }
+
+  // Define a new BorderPanel
+  val bp: BorderPanel = new BorderPanel() {
+
+    // Create a new FlowPanel from the createButtonPane method on the lazy loaded top Frame above
+    val component: Component = top.gameLauncher.createButtonPane(top)
+
+    // Map the component to a constraint
+    layout(component) = BorderPanel.Position.South
+  }
+
+  // Define the main frame contents
+  top.contents = bp
+}
+
+
+// Represents a window from which new games can be launched
 class LaunchGame {
+
+  // Creates a new game instance/window
   def showNewWindow() = {
-    val frame: Frame = new GameFrame()
+    new GameFrame()
   }
 
   def createButtonPane(f: Frame): FlowPanel = {
@@ -15,6 +40,7 @@ class LaunchGame {
     f.reactions += {
       case ButtonClicked(`button`) => showNewWindow()
     }
+
     new FlowPanel() {
       border = Swing.EmptyBorder(5,5,5,5)
       contents += button
@@ -22,7 +48,8 @@ class LaunchGame {
   }
 }
 
-
+// Represents a new game window and instance of which is created by the showNewWindow method
+// which is called from a listener on the Launch Game Button
 class GameFrame extends Frame {
   title = "Battleships"
   val button = new Button {
@@ -35,30 +62,7 @@ class GameFrame extends Frame {
 }
 
 
-object LaunchGame extends SimpleSwingApplication {
-  lazy val top: Frame { val demo: LaunchGame } = new Frame() {
-    title = "FrameDemo2"
-    //Use the Java look and feel.  This needs to be done before the frame is created
-    //so the companion object FrameDemo2 cannot simply extend SimpleSwingApplcation.
-    try {
-      UIManager.setLookAndFeel(
-        UIManager.getCrossPlatformLookAndFeelClassName());
-    } catch {
-      case e: Exception => ;
-    }
-    //Make sure we have nice window decorations.
-    JFrame.setDefaultLookAndFeelDecorated(true);
-    JDialog.setDefaultLookAndFeelDecorated(true);
-    //Create and set up the content pane.
-    val demo = new LaunchGame()
-  }
 
-  val bp: BorderPanel = new BorderPanel() {
-    layout(top.demo.createButtonPane(top)) = BorderPanel.Position.South
-  }
-  top.contents = bp
-
-}
 
 
 
